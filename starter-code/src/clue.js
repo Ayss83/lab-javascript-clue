8198a962f96d78afc7abce301e7644c60c6eb7d7
Renaud <renoruns@gmail.com>
2018-03-21 12:27:31 +0100

    done

--- starter-code/src/clue.js
+++ starter-code/src/clue.js
@@ -8,7 +8,6 @@ description:  He has a lot of connections
 age:          45
 image:        https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg
 occupation:   Entrepreneur
-
 drOrchid
 frst_name:   Doctor
 lst_name:    Orchid
@@ -17,7 +16,6 @@ description:  PhD in plant toxicology. Adopted daughter of Mr. Boddy
 age:          26
 image:        http://www.radiotimes.com/uploads/images/Original/111967.jpg
 ocupation:   Scientist
-
 profPlum
 first_name:   Victor
 last_name:    Plum
@@ -26,7 +24,6 @@ description:  Billionare video game designer
 age:          22
 image:        https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg
 occupation:   Designer
-
 missScarlet
 first_name:   Kasandra
 last_name:    Scarlet
@@ -35,7 +32,6 @@ description:  She is an A-list movie star with a dark past
 age:          31
 image:        https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg
 occupation:   Actor
-
 mrsPeacock
 first_name:   Eleanor
 last_name:    Peacock
@@ -44,7 +40,6 @@ description:  She is from a wealthy family and uses her status and money to earn
 age:          36
 image:        https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg
 occupation:   Socialité
-
 mrMustard
 first_name:   Jack
 last_name:    Mustard
@@ -53,9 +48,7 @@ description:  He is a former football player who tries to get by on his former g
 age:          62
 image:        https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg
 occupation:   Retired Football player
-
 // Weapons
-
 name: rope        ---   weight: 10
 name: knife       ---   weight: 8
 name: candlestick ---   weight: 2
@@ -65,7 +58,6 @@ name: axe         ---   weight: 15
 name: bat         ---   weight: 13
 name: trophy      ---   weight: 25
 name: pistol      ---   weight: 20
-
 // Rooms
 name: Dinning Room
 name: Conservatory
@@ -83,12 +75,149 @@ name: Theater
 name: Guest House
 name: Patio
 */
-
+var mrGreen = {
+  first_name:   'Jacob',
+  last_name:    "Green",
+  color:        "green",
+  description:  "He has a lot of connections",
+  age:          "45",
+  image:        'https://pbs.twimg.com/profile_images/506787499331428352/65jTv2uC.jpeg',
+  occupation:   "Entrepreneur",
+  }
+var drOrchid = {
+  frst_name:   "Doctor",
+  lst_name:    "Orchid",
+  color:        "white",
+  description:  "PhD in plant toxicology. Adopted daughter of Mr. Boddy",
+  age:          "26",
+  image:        'http://www.radiotimes.com/uploads/images/Original/111967.jpg',
+  ocupation:   'Scientist'
+}
+var profPlum = {
+  first_name:   "Victor",
+  last_name:    "Plum",
+  color:        "purple",
+  description:  "Billionare video game designer",
+  age:          "22",
+  image:        'https://metrouk2.files.wordpress.com/2016/07/professor-plum.jpg',
+  occupation:   "Designer"
+}
+var missScarlet = {
+  first_name:   "Kasandra",
+  last_name:    "Scarlet",
+  color:        "red",
+  description:  "She is an A-list movie star with a dark past",
+  age:          "31",
+  image:        'https://metrouk2.files.wordpress.com/2016/07/miss-scarlett.jpg',
+  occupation:   "Actor"
+}
+var mrsPeacock = {
+  first_name:   "Eleanor",
+  last_name:    "Peacock",
+  color:        "blue",
+  description:  "She is from a wealthy family and uses her status and money to earn popularity",
+  age:          "36",
+  image:        "https://metrouk2.files.wordpress.com/2016/07/mrs-peacock.jpg",
+  occupation:   "Socialité"
+ }
+var mrMustard = {
+  first_name:   "Jack",
+  last_name:    "Mustard",
+  color:        "yellow",
+  description:  "He is a former football player who tries to get by on his former glory",
+  age:          "62",
+  image:        "https://metrouk2.files.wordpress.com/2016/07/colonel-mustard.jpg",
+  occupation:   "Retired Football player"
+ }
+ 
+var rope = {name: "rope",weight: 10};
+var knife = {name: "knife",weight: 8};
+var candlestick = {name: "candlestick", weight: 2};
+var dumbell = {name: "dumbbell", weight: 30};
+var poison = {name: "poison", weight: 2};
+var axe = {name: "axe", weight: 15};
+var bat = {name: "bat", "weight": 13};
+var trophy = {name: "trophy", weight: 25};
+var pistol = {name: "pistol", weight: 20};
+var diningRoom = {
+  name: "Dinning Room"
+ }
+ 
+ var conservatory = {
+  name: "Conservatory"
+ }
+ 
+ var kitchen = {
+  name: "Kitchen"
+ }
+ 
+ var study = {
+  name: "Study"
+ }
+ 
+ var library = {
+  name: "Library"
+ }
+ 
+ var billiard = {
+  name: "Billiard Room"
+ }
+ 
+ var lounge = {
+  name: "Lounge"
+ }
+ 
+ var ballroom = {
+  name: "Ballroom"
+ }
+ 
+ var hall = {
+  name: "Hall"
+ }
+ 
+ var spa = {
+  name: "Spa"
+ }
+ 
+ var livingRoom = {
+  name: "Living Room"
+ }
+ 
+ var observatory = {
+  name: "Observatory"
+ }
+ 
+ var theater = {
+  name: "Theater"
+ }
+ 
+ var guestHouse = {
+  name: "Guest House"
+ }
+ 
+ var patio = {
+  name: "Patio"
+ }
 // Characters Collection
-var charactersArray = [];
-
+var charactersArray = [mrGreen, drOrchid, profPlum, missScarlet, mrsPeacock, mrMustard];
 // Rooms' Collection
-var roomsArray = [];
-
+var roomsArray = [diningRoom, conservatory, kitchen, study, library, billiard, lounge, ballroom, hall, spa, livingRoom, observatory, theater,, guestHouse, patio];
 // Weapons Collection
-var weaponsArray = [];
+var weaponsArray = [rope, knife, candlestick, dumbell, poison, axe, bat, trophy, pistol];
+function randomSelector (array){
+  var random = Math.floor(Math.random()*array.length);
+  return array[random];
+}
+function pickMistery(){
+  res = [];
+  res.push(randomSelector(charactersArray));
+  res.push(randomSelector(weaponsArray));
+  res.push(randomSelector(roomsArray));
+  return res;
+}
+function revealMistery(misteryEnvelope){
+  misteryCharacter = misteryEnvelope[0];
+  misteryWeapon = misteryEnvelope[1];
+  misteryRoom = misteryEnvelope[2];
+  return (misteryCharacter.first_name + ' ' + misteryCharacter.last_name + ' killed Mr.Boddy using the ' + misteryWeapon.name + ' in the ' + misteryRoom.name + '!!!!')
+}
